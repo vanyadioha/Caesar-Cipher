8119aa0000dfb726e12063ad99b773256b4ec8db
Victor Anyadioha <victoranyadioha@gmail.com>
2023-08-23 19:14:16 +0100

    Chore: Cleaned up redundant code

--- abcd.py
+++ abcd.py
@@ -1,4 +1,3 @@
 alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 alphabet_list = alphabet.split()
-print(26 + (-52 - 26) % -26)
